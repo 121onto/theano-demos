4444939b284e61b49c1d5f7d46c70a52f2db499d
121onto <121onto@gmail.com>
2015-11-03 15:48:43 -0500

    Refactor a few things

---
+++ code/utils.py
@@ -0,0 +1,49 @@
+from __future__ import (print_function, division)
+
+import numpy as np
+import numpy.random as rng
+import pandas as pd
+import theano
+import theano.tensor as T
+
+###########################################################################
+## i/o
+
+def save_params(params, path=MODEL_PATH):
+    with open(path, 'wb') as file:
+        for v in params:
+            cPickle.dump(v.get_value(borrow=True), save_file, -1)
+
+def load_params(params, path=MODEL_PATH):
+    with open(path) as file:
+        for v in params:
+            v.set_value(cPickle.load(save_file), borrow=True)
+
+###########################################################################
+## shared dataset
+
+def shared_dataset(data):
+    """
+    Function that loads the dataset into shared variables.
+
+    Parameters
+    ----------
+    data: tuple of numpy arrays
+      data = (x, y) where x is an np.array of predictors and y is an np array
+      of outcome variables
+
+    """
+    x, y = data
+    sx = theano.shared(np.asarray(x, dtype=theano.config.floatX))
+    sy = theano.shared(np.asarray(y, dtype=theano.config.floatX))
+    return sx, T.cast(sy, 'int32')
+
+
+def test_shared_dataset():
+    train, valid, test = None, None, None
+    with gzip.open('data/mnist.pkl.gz', 'rb') as f:
+        train, valid, test = cPickle.load(f)
+
+    tn_x, tn_y = shared_dataset(train)
+    v_x , v_y  = shared_dataset(valid)
+    tt_x, tt_y = shared_dataset(test)
