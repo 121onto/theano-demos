be9501062bbcaa870447a2d84bb1450f781bdd82
121onto <121onto@gmail.com>
2015-11-03 20:34:51 -0500

    Give layer classes their own file.  Debug.

---
+++ code/logistic_regression_basic.py
@@ -0,0 +1,68 @@
+from __future__ import (print_function, division)
+
+import timeit
+import gzip
+import cPickle
+import numpy as np
+import numpy.random as rng
+import pandas as pd
+import theano
+import theano.tensor as T
+
+###########################################################################
+## local imports
+
+from code.utils import (shared_dataset, load_params, save_params)
+
+###########################################################################
+## Logistic function
+
+def logistic_function():
+    x = T.dmatrix('x')
+    s = 1 / (1 + T.exp(-x))
+    logistic = theano.function([x], s)
+    print(logistic([[0, 1], [-1, -2]]))
+    """
+    array([[ 0.5       ,  0.73105858],
+           [ 0.26894142,  0.11920292]])
+    """
+
+###########################################################################
+## Logistic regression with random data
+
+def logistic_regression_random_data(N=500, feats=700, training_steps=10000):
+    # data
+    data = dict(
+        x = rng.randn(N, feats),
+        y = rng.randint(size=N, low=0, high=2)
+    )
+
+    # symbolic variables
+    x = T.matrix('x')
+    y = T.vector('y')
+    w = theano.shared(rng.randn(feats), name='w')
+    b = theano.shared(0., name='b')
+
+    # functions
+    p_1 = 1/(1 + T.exp(-T.dot(x, w) - b))
+    prediction = p_1 > 0.5
+    cross_entropy = -y * T.log(p_1) - (1-y) * T.log(1-p_1)
+    cost = cross_entropy.mean() + 0.01 * (w ** 2).sum() # regularization
+    dw, db = T.grad(cost, [w, b])
+
+    train = theano.function(
+        inputs = [x, y],
+        outputs = [prediction, cost],
+        updates = ((w, w - 0.1 * dw), (b, b - 0.1 * db))
+    )
+    predict = theano.function(inputs=[x], outputs=prediction)
+
+    pred, err = train(data['x'], data['y'])
+    print('Cost before fitting:')
+    print(err)
+
+    for i in range(training_steps-1):
+        pred, err = train(data['x'], data['y'])
+
+    print('Cost at solution:')
+    print(err)
--- code/utils.py
+++ code/utils.py
@@ -1,5 +1,6 @@
 from __future__ import (print_function, division)
 
+import timeit
 import cPickle
 import numpy as np
 import numpy.random as rng
@@ -44,3 +45,63 @@ def shared_dataset(data, borrow=True):
         borrow=borrow
     )
     return sx, T.cast(sy, 'int32')
+
+###########################################################################
+## solvers
+
+def fit_msgd_early_stopping(datasets, n_batches, models, classifier, outpath,
+                   n_epochs=1000, patience=5000, patience_increase=2,
+                   improvement_threshold=0.995):
+
+    # unpack parameters
+    [(tn_x, tn_y), (v_x, v_y), (tt_x, tt_y)] = datasets
+    n_tn_batches, n_v_batches, n_tt_batches = n_batches
+    tn_model, v_model, tt_model = models
+
+    validation_frequency = min(n_tn_batches, patience/2)
+
+    # initialize some variables
+    best_validation_loss = np.inf
+    best_iter = 0
+    test_score = 0.
+    start_time = timeit.default_timer()
+
+    # main loop
+    done_looping = False
+    epoch = 0
+    while (epoch < n_epochs) and (not done_looping):
+        epoch = epoch + 1
+        for minibatch_index in xrange(n_tn_batches):
+            minibatch_avg_cost = tn_model(minibatch_index)
+            iter = (epoch - 1) * n_tn_batches + minibatch_index
+
+            if (iter + 1) % validation_frequency == 0:
+                validation_losses = [v_model(i) for i in xrange(n_v_batches)]
+                this_validation_loss = np.mean(validation_losses)
+                print(
+                    'epoch %i, minibatch %i/%i, validation error %f %%' %
+                    (
+                        epoch,
+                        minibatch_index + 1,
+                        n_tn_batches,
+                        this_validation_loss * 100.
+                    )
+                )
+
+                if this_validation_loss < best_validation_loss:
+                    if this_validation_loss < best_validation_loss * improvement_threshold:
+                        patience = max(patience, iter * patience_increase)
+
+                    #best_params = copy.deepcopy(params)
+                    best_validation_loss = this_validation_loss
+                    best_iter = iter
+
+
+            if patience <= iter:
+                done_looping = True
+                break
+
+        save_params(classifier.params, path=outpath)
+
+    end_time = timeit.default_timer()
+    return best_validation_loss, best_iter, epoch, (end_time - start_time)
